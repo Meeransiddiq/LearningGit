e5c28cade9004768d524bb65efc602a283c70ad6
Meeran Siddiqui <mee.sidd.1919@gmail.com>
2023-04-03 01:08:52 +0530

    adding multiply.java

---
+++ multiply.java
@@ -0,0 +1,9 @@
+class multiply{
+    public static void main(String args[])
+    {
+        int a ,b;
+           a=10;
+           b=40;
+           System.out.println("Product of two number :"+(a*b));
+    }
+}
